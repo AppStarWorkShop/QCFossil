5ceaa08107bd96dcb134a1e81dd3ba8fca0eb4e9
Daniel <yamado.kira@gmail.com>
2022-06-19 22:10:43 +0800

    fix qc info page UI issue

--- QCFossil/ViewController.swift
+++ QCFossil/ViewController.swift
@@ -128,7 +128,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, UITextField
         //self.password.text = "wE$6T+8a"
         
         let defaults = UserDefaults.standard
-        let releaseDate = "20220616"
+        let releaseDate = "20220619"
         _RELEASE = releaseDate as String
         defaults.set(releaseDate, forKey: "release_preference")
         
